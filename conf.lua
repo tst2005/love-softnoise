9752cf59332becddaf31b08fb6d3fe9f52435387
TsT <tst2005@gmail.com>
2015-10-25 13:55:25 +0100

    disable the console on Windows

--- conf.lua
+++ conf.lua
@@ -3,13 +3,16 @@ function love.conf(t)
 	t.author = "TsT"		-- The author of the game (string)
 	t.identity = "softnoise"	-- The name of the save directory (string)
 	t.version = "0.9.1"		-- The LÖVE version this game was made for (number)
-	t.console = true            -- Attach a console (boolean, Windows only)
-	t.screen = {}
+	t.console = false		-- Attach a console (boolean, Windows only)
+
+	t.screen = t.screen or {}
 --	t.screen.width = 800		-- The window width (number)
 --	t.screen.height = 600		-- The window height (number)
 	t.screen.fullscreen = false -- Enable fullscreen (boolean)
 	t.screen.vsync = false      -- Enable vertical sync (boolean)
 	t.screen.fsaa = 0           -- The number of FSAA-buffers (number)
+
+	t.modules = t.modules or {}
 	t.modules.joystick = false	-- Enable the joystick module (boolean)
 	t.modules.audio = true		-- Enable the audio module (boolean)
 	t.modules.keyboard = true	-- Enable the keyboard module (boolean)
